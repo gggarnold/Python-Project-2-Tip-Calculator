3bc2b524e6724d7bf8f30f9fcb88fb5dacec9ddb
gggarnold <107363732+gggarnold@users.noreply.github.com>
2023-08-28 12:59:07 +0000

    Tip Calculator

--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
 
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
+#TIP CALCULATOR
 
 print("Welcome to the tip calculator")
 
@@ -22,3 +20,4 @@ tipanswer = (totalbill_as_float / billsplit_as_int) * percentagetip_as_int
 tipanswerfloat = ("%.2f" % tipanswer)
 
 print(f"Each person should pay ${tipanswerfloat}")
+
